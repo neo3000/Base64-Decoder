090f3be046feaf074584caaf4ca16846e413c60a
neo3000 <neo031989@gmail.com>
2012-02-19 00:57:32 +0100

    Base64-Decoder in vb.net Signed-off-by: neo3000 <neo031989@gmail.com>

---
+++ Base64-Decode/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        SaveFileDialog1.ShowDialog()
+        TextBox1.Text = SaveFileDialog1.FileName
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Try
+            Dim src As String = RichTextBox1.Text
+            Dim bt64 As Byte() = System.Convert.FromBase64String(src)
+            If IO.File.Exists(TextBox1.Text) Then
+                IO.File.Delete(TextBox1.Text)
+            End If
+            Dim sw As New IO.FileStream(TextBox1.Text, IO.FileMode.Create)
+            sw.Write(bt64, 0, bt64.Length)
+            sw.Close()
+            MsgBox("Fertig")
+        Catch ex As Exception
+            MsgBox(ex.ToString())
+        End Try
+    End Sub
+End Class
---
+++ Base64-Decode/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("Base64Decoder")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Koch")> 
+<Assembly: AssemblyProduct("Base64Decoder")> 
+<Assembly: AssemblyCopyright("Copyright © MKoch2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("7537270c-5089-4df8-82bd-6f0d824efc0c")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
